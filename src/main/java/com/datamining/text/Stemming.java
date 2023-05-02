db1b1c7926f3e0052c26a394ec734ff6c5617244
Benjamin Vergara <aquantumguy@gmail.com>
2023-05-01 20:10:01 -0600

    Step 1a, 1b

--- src/main/java/com/datamining/text/Stemming.java
+++ src/main/java/com/datamining/text/Stemming.java
@@ -1,11 +1,20 @@
 package com.datamining.text;
 
+import com.google.common.primitives.Chars;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Stemming {
 
     public String word;
+    private int wordLength;
+    StringBuilder wordM = new StringBuilder();
 
-    public static final  char [] VOWELS={'a','e', 'i', 'o', 'u', 'y'};
-    public static final  String [] DOBLE={"bb", "dd", "ff" , "gg",   "mm" ,"nn","pp","rr","tt"};
+
+
+    public static final char[] VOWELS= {'a', 'e', 'i', 'o', 'u', 'y'};
+    public static final  String [] DOUBLE={"bb", "dd", "ff" , "gg",   "mm" ,"nn","pp","rr","tt"};
     public static final  char [] LI_ENDING={'e', 'g','d','c','h','k','n','m','r','t'};
 
     private String r1;
@@ -48,18 +57,86 @@ public class Stemming {
 
     }
 
-    public void step1a(){
+    private void step1a(){
+
+        calculateM(word);
+
+        if(word.endsWith("s")){
+            if(word.endsWith("sses")) wordLength -= 2;
+            if(word.endsWith("ies")){
+                if(wordLength == 4) wordLength -= 1;
+                else wordLength -= 2;
+                //kiwis
+            }else if (wordLength > 3 && word.charAt(wordLength-2) != 's' && word.charAt(wordLength-2) != 'u' && word.charAt(wordLength-2) != 'x'
+                    && (word.charAt(wordLength-2) != 'i')) {
+                wordLength --;
+            }
+        }
+        word = word.substring(0,wordLength);
+    }
+
+    private void step1b(){
+
+        if(word.endsWith("eed") && calculateM(word.substring(0,wordLength-3)) > 0) {wordLength -= 1; word = word.substring(0,wordLength);}
+        if(word.endsWith("ed") && containsVowel(word.substring(0,wordLength-2))) {wordLength -= 2;  word = word.substring(0,wordLength);  step1bExtension();;}
+        if(word.endsWith("ing") && containsVowel(word.substring(0,wordLength-3))) {wordLength -= 3;  word = word.substring(0,wordLength);  step1bExtension();}
+
+
+
 
     }
 
+    private void step1bExtension(){
+        if(word.endsWith("at") || word.endsWith("bl") || word.endsWith("iz")) {word = word.concat("e");wordLength++;}
+        //Checks if they are double consonants
+        if(!isVowel(word.charAt(wordLength-1)) && word.charAt(wordLength-1) == word.charAt(wordLength-2)){
+            if(!word.endsWith("l") && !word.endsWith("s") &&  !word.endsWith("z")) {wordLength--; word = word.substring(0,wordLength);};
+        }
+        if(calculateM(word) == 1 && isO()) { word = word.concat("e"); wordLength++;};
 
+    }
+
+    private void step2(){
+
+    }
+
+    private boolean containsVowel(String word){
+        for(int i = 0; i < word.length(); i++){
+            if(isVowel(word.charAt(i))) return true;
+        }
+        return false;
+    }
+
+    private boolean isO(){
+        return wordM.charAt(wordLength-1) == 'C' && wordM.charAt(wordLength - 2) == 'V' && wordM.charAt(wordLength - 3) == 'C'
+                && word.charAt(wordLength-1) != 'w' &&  word.charAt(wordLength-1) != 'x' &&  word.charAt(wordLength-1) != 'y';
+    }
+    private int calculateM(String word){
+
+        int m = 0;
+
+
+        wordLength = word.length();
+
+        for(int i=0; i< wordLength; i++){
+            if(isVowel(word.charAt(i))) wordM.append('V');
+            else wordM.append('C');
+        }
+
+        for(int i=0; i<wordLength-1; i++){
+            if(wordM.charAt(i) == 'V' && wordM.charAt(i+1) == 'C') m++;
+        }
+
+        return m;
+
+    }
 
     public static void main(String[] args) {
         Stemming a=new Stemming();
-        a.word="yes";
-        a.Y();
+        a.word="hoping";
+        a.step1a();
+        a.step1b();
         System.out.println(a.word);
-
     }
 
 

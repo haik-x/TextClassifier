339f5e19a7bf8d3be59b3643623d8491bbaa00cb
SamQuintero <samabyq@gmail.com>
2023-05-14 17:26:25 -0600

    Identation

--- src/main/java/com/datamining/classifier/DistanceLabel.java
+++ src/main/java/com/datamining/classifier/DistanceLabel.java
@@ -54,7 +54,7 @@ public class DistanceLabel implements Comparable<DistanceLabel> {
             return -1;
         }
     }
-    
+
     @Override
     public String toString() {
         return String.format("%s: %f", this.label, this.distance);
